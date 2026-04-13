a62ddaf5f0a4d26f6767a3604b3bcab5c912c407
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2026-04-13 18:31:44 +0200

    stack-services: providers file for services stack

---
+++ stacks/services/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.11.0"
+  
+  required_providers {
+    proxmox = {
+      source = "registry.opentofu.org/bpg/proxmox"
+      version = "~> 0.101.1"
+    }  
+  }
+}
+
+provider "proxmox" {
+  endpoint = var.proxmox_endpoint
+  api_token = var.proxmox_api_token
+  insecure = true
+}
