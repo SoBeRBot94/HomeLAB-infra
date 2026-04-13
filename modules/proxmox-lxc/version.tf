2d4ee192500e180a3ca8e79c56c2fbd90ae7cd95
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2026-04-13 18:22:07 +0200

    proxmox-lxc: opentofu proxmox lxc module

---
+++ modules/proxmox-lxc/version.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.11.0"
+
+  required_providers {
+    proxmox = {
+      source = "registry.opentofu.org/bpg/proxmox"
+      version = "~> 0.101"
+    }
+  }
+}
