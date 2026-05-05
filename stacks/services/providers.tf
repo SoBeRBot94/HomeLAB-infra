10e772931549998c01278c48e702da5cd2e727b2
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2026-05-05 17:13:34 +0200

    stack-services: fixed liniting in syncthing stack service file and providers file

--- stacks/services/providers.tf
+++ stacks/services/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_version = ">= 1.11.0"
   
   required_providers {
-    proxmox = {
-      source = "registry.opentofu.org/bpg/proxmox"
+    proxmox   = {
+      source  = "registry.opentofu.org/bpg/proxmox"
       version = "~> 0.101.1"
     }  
   }
 }
 
 provider "proxmox" {
-  endpoint = var.proxmox_endpoint
+  endpoint  = var.proxmox_endpoint
   api_token = var.proxmox_api_token
-  insecure = true
+  insecure  = true
 }
